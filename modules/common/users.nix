3f7bc090972591ffc916110cd3f89cb0095f7ba1
Trizotto <julien.roux404@gmail.com>
2025-05-07 00:11:52 +0200

    add some settings in openssh

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -7,5 +7,18 @@
     description = "Trizotto";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [ ];
+    openssh.authorizedKeys.keys = [
+       #"ssh-ed25519 <some-public-key> blabla@blabla"
+    ];
   };
+
+  #services.openssh = {
+  #  enable = true;
+  #  settings = {
+  #    X11Forwarding = true;
+  #    PermitRootLogin = "no"; # disable root login
+  #    PasswordAuthentication = false; # disable password login
+  #  };
+  #  openFirewall = true;
+  #};
 }
