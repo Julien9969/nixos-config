57ae5e5503604adcbb054484ec8c90efa93c2962
Trizotto <julien.roux404@gmail.com>
2025-05-11 08:55:47 +0200

    move NVIDIA configuration for graphics support and driver management

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -3,6 +3,7 @@
   imports = [
     ./hardware-configuration.nix
     ./hard-drives.nix
+    ./nvidia.nix
     ../../modules/common/firewall.nix
     ../../modules/common/users.nix
     ../../modules/common/nix.nix
@@ -11,12 +12,15 @@
     ../../modules/services/docker.nix
     ../../modules/services/openssh.nix
     ../../modules/services/jellyfin.nix
+    # ../../modules/services/cockpit.nix
     ../../modules/services/reverse-proxy.nix
   ];
 
   networking.hostName = "trizottoserver";
   
   environment.variables.EDITOR = "micro";
+  zramSwap.enable = true;
+  powerManagement.cpuFreqGovernor = "performance";
 
   # needed for vscode-server - allowing foreign binaries to run on NixOS
   programs.nix-ld.enable = true;
@@ -33,6 +37,8 @@
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
+
+    # cockpit
   ];
 
   system.autoUpgrade = {
@@ -56,62 +62,16 @@
       dates = "weekly";
       options = "--delete-older-than 30d";
     };
+    package = pkgs.nixFlakes;
+    settings = {
+      experimental-features = "nix-command flakes repl-flake";
+      auto-optimise-store = true;
+    };
   };
 
   # Don't sleep on lid close
   services.logind.lidSwitchExternalPower = "ignore";
-
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
-
-  # Nvida driver
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-	prime = {
-      offload.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-    
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  environment.variables = {
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  };
-
-  
+ 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
