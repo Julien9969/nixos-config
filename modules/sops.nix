25412ce25d27736a8fa966fe199fe30d59f0fd80
Trizotto <julien.roux404@gmail.com>
2025-05-13 05:44:54 +0200

    chore: indent in sops.nix

--- modules/sops.nix
+++ modules/sops.nix
@@ -2,18 +2,18 @@
 { config, pkgs, ... }:
 {
   sops = {
-  defaultSopsFile = ../secrets/secrets.yaml;
-  defaultSopsFormat = "yaml";
-  age.keyFile = config.users.users.trizotto.home + "/.config/sops/age/keys.txt";
-  # age.sshKeyPaths = ["~/.ssh/id_ed25519"];
-  secrets = {
-      "authorized_keys" = {
-        path = "${config.users.users.trizotto.home}/.ssh/authorized_keys"; 
-        owner = config.users.users.trizotto.name;                      
-        group = config.users.users.trizotto.group;                     
-        mode = "0600";
-      };
-      "username" = {};
-  };
+    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = config.users.users.trizotto.home + "/.config/sops/age/keys.txt";
+    # age.sshKeyPaths = ["~/.ssh/id_ed25519"];
+    secrets = {
+        "authorized_keys" = {
+          path = "${config.users.users.trizotto.home}/.ssh/authorized_keys"; 
+          owner = config.users.users.trizotto.name;                      
+          group = config.users.users.trizotto.group;                     
+          mode = "0600";
+        };
+        "username" = {};
+    };
   };
 }
