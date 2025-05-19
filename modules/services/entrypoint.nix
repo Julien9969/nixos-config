382651ac2f9a08743b44f9e29af01079e1108f42
Trizotto <julien.roux404@gmail.com>
2025-05-19 04:19:45 +0200

    feat: add Zipline service configuration (disabled)

--- modules/services/entrypoint.nix
+++ modules/services/entrypoint.nix
@@ -9,10 +9,12 @@
     ../../modules/services/reverse-proxy.nix
     ../../modules/services/cockpit.nix
     ../../modules/services/qbit-run.nix
+    ../../modules/services/zipline.nix
   ];  
 
   config.services.myServices.enableCockpit = false;
   config.services.myServices.enableJellyfin = true;
   config.services.myServices.servarr.enableSonarr = false;
-  config.services.myServices.servarr.enableRadarr = false;
+  config.services.myServices.servarr.enableRadarr = false;  
+  config.services.myServices.enableZipline = false;
 }
---
+++ modules/services/zipline.nix
@@ -0,0 +1,32 @@
+# modules/services/zipline.nix
+{ config, lib, pkgs, ... }:
+let
+  enabled = config.services.myServices.enableZipline or false;
+in
+{
+  options.services.myServices.enableZipline = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = '''
+      Enable the Zipline service
+      https://zipline.diced.sh/
+    '';
+  };
+
+  config = lib.mkIf enabled {
+    services.zipline = {
+      enable = true;
+      settings = {
+        CORE_HOSTNAME = "0.0.0.0";
+        CORE_PORT = 3000;
+        CORE_SECRET = "Secret (echo -n "xxx" | md5sum)";
+        DATASOURCE_LOCAL_DIRECTORY = "/var/lib/zipline/uploads";
+        DATASOURCE_TYPE = "local";
+      };
+    };
+
+    networking.firewall = {
+      allowedTCPPorts = [ 3000 ];
+    };
+  };
+}
