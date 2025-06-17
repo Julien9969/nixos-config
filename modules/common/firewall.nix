a64a0d9095c9b59202b19b50287df1356c83c722
Trizotto <julien.roux404@gmail.com>
2025-06-18 00:08:04 +0200

    feat: add nameservers and disable DNS management by NetworkManager

--- modules/common/firewall.nix
+++ modules/common/firewall.nix
@@ -1,6 +1,14 @@
 # modules/common/firewall.nix
 { config, pkgs, ... }:
 {
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::9"
+    "2620:fe::10"
+  ];
+  networking.networkmanager.dns = "none"; # Empêche NM de gérer le DNS
+  
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.allowedUDPPorts = [ 80 443 ];
