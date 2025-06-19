f0b7b7cfa2dd5f26c1f128c5791d0cda383e3b08
Trizotto <julien.roux404@gmail.com>
2025-06-19 18:42:10 +0200

    feat: move virtualhost function and nginx conf to lib

---
+++ lib/mk-virtualhost/default.nix
@@ -0,0 +1,15 @@
+# lib/mkVirtualHost.nix
+{ locations, 
+  useACMEHost, 
+  forceSSL ? true, 
+  extraConfig ? "", 
+  blockCommonExploit ? false, 
+  cacheAssets ? false }:
+{
+  inherit locations forceSSL useACMEHost;
+  extraConfig = ''
+    ${if blockCommonExploit then "include ${./block-exploits.conf}" else ""};
+    ${if cacheAssets then "set $proxyPass ${locations."/".proxyPass};\ninclude ${./cache-asset.conf};" else ""}
+    ${extraConfig}
+  '';
+}
