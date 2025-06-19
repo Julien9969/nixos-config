b1e644d31b4a214644ce6960ab16b968a41fb253
Trizotto <julien.roux404@gmail.com>
2025-06-19 19:40:26 +0200

    fix: if blockCommonExploit is false, incorrect ';' position makes nginx crash

--- lib/mk-virtualhost/default.nix
+++ lib/mk-virtualhost/default.nix
@@ -8,7 +8,7 @@
 {
   inherit locations forceSSL useACMEHost;
   extraConfig = ''
-    ${if blockCommonExploit then "include ${./block-exploits.conf}" else ""};
+    ${if blockCommonExploit then "include ${./block-exploits.conf};" else ""}
     ${if cacheAssets then "set $proxyPass ${locations."/".proxyPass};\ninclude ${./cache-asset.conf};" else ""}
     ${extraConfig}
   '';
