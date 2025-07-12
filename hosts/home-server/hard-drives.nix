cfd212fa6e85f4ba2e2008b774685703ee8f406e
Trizotto <julien.roux404@gmail.com>
2025-07-12 16:26:42 +0200

    feat: change drive to ext4

--- hosts/home-server/hard-drives.nix
+++ hosts/home-server/hard-drives.nix
@@ -1,30 +1,36 @@
 { config, lib, pkgs, ... }:
 {
-  boot.supportedFilesystems = [ "ntfs" ];
+  # boot.supportedFilesystems = [ "ntfs" ];
+  
+  systemd.tmpfiles.settings.root."/media".z = { # z fix the perms event if folder exist (d not)
+    user = "root";
+    group = "media";
+    mode = "0775"; 
+  };
 
-  fileSystems."/media/HDD" = {
+  fileSystems."/media/DSK" = {
     device = "/dev/disk/by-uuid/E26E78BA6E7888D5";
-    fsType = "ntfs-3g";
+    fsType = "ext4";
     options = [
-      "uid=1000"    # main user id
-      "gid=${toString config.users.groups.media.gid}"  # group = media
-      "umask=002"
-      "dmask=002"   # Directories = 775
-      "fmask=113"   # Files = 664
-      "allow_other"
       "nofail"
     ];
   };
 
+  systemd.tmpfiles.settings.root."/media/DSK".z = {
+    user = "root";
+    group = "media";
+    mode = "0775"; 
+  };
+
   services.udev.extraRules = 
-   let
-     mkRule = as: lib.concatStringsSep ", " as;
-     mkRules = rs: lib.concatStringsSep "\n" rs;
-   in mkRules ([( mkRule [
-     ''ACTION=="add|change"''
-     ''SUBSYSTEM=="block"''
-     ''KERNEL=="sd[a-z]"''
-     ''ATTR{queue/rotational}=="1"''
-     ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k1"''
-   ])]); 
+    let
+      mkRule = as: lib.concatStringsSep ", " as;
+      mkRules = rs: lib.concatStringsSep "\n" rs;
+    in mkRules ([( mkRule [
+      ''ACTION=="add|change"''
+      ''SUBSYSTEM=="block"''
+      ''KERNEL=="sd[a-z]"''
+      ''ATTR{queue/rotational}=="1"''
+      ''RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 41 /dev/%k1"''
+    ])]); 
 }
