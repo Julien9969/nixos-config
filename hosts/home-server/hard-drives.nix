aa7cc26ec9fa8304c690980ab73c2894f5eb44a8
Trizotto <julien.roux404@gmail.com>
2025-07-17 19:42:09 +0200

    feat : config to connect all drives

--- hosts/home-server/hard-drives.nix
+++ hosts/home-server/hard-drives.nix
@@ -5,10 +5,28 @@
   systemd.tmpfiles.rules = [
     "d /media 0775 root media - -"
     "d /media/DSK 0775 root media - -"
+    "d /media/NAS 0775 root media - -"
+    "d /media/EXOS 0775 root media - -"
   ];
 
   fileSystems."/media/DSK" = {
-    device = "/dev/disk/by-uuid/E26E78BA6E7888D5";
+    device = "/dev/disk/by-uuid/a4cac6f4-51c7-4378-a608-c5710e5ae31a";
+    fsType = "ext4";
+    options = [
+      "nofail"
+    ];
+  };
+
+  fileSystems."/media/NAS" = {
+    device = "/dev/disk/by-uuid/728adc7c-630e-4ef5-9c77-368d5c500eac";
+    fsType = "ext4";
+    options = [
+      "nofail"
+    ];
+  };
+
+  fileSystems."/media/EXOS" = {
+    device = "/dev/disk/by-uuid/335fff74-08ab-4dad-9cb7-9d719106076c";
     fsType = "ext4";
     options = [
       "nofail"
