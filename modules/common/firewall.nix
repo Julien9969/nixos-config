33a39daa077c9efcd6a425d597a96b355be57aed
Trizotto <julien.roux404@gmail.com>
2025-11-03 18:57:05 +0100

    feat: update firewall configuration and clean up unused settings

--- modules/common/firewall.nix
+++ modules/common/firewall.nix
@@ -1,43 +1,42 @@
 { config, pkgs, ... }:
+
 {
   networking.useDHCP = false;
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.enableIPv6 = false;
 
+  # === Ethernet (LAN) - Primary ===
   networking.interfaces.enp3s0f1 = {
     ipv4.addresses = [{
       address = "192.168.1.200";
       prefixLength = 24;
     }];
-    ipv6.addresses = [{
-      address = "2a01:e0a:99b:8820::200"; 
-      prefixLength = 64;
+    
+    ipv4.routes = [{
+      address = "0.0.0.0";
+      prefixLength = 0;
+      via = "192.168.1.254";
+      options = { metric = "100"; }; # Lower number = higher priority
     }];
   };
 
-  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  # === Wi-Fi (Fallback) ===
+  networking.interfaces.wlp2s0 = {
+    useDHCP = true;
+  };
 
   services.resolved = {
     enable = true;
     dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
     dnsovertls = "true";
+    fallbackDns = [ "8.8.4.4" ];
   };
-
-  networking.interfaces.wlp2s0.useDHCP = true;
-
-  networking.defaultGateway = { 
-    address =  "192.168.1.254";
-    interface = "enp3s0f1";
-  };
-
-  # Needed for rstp live TV
-  networking.firewall.trustedInterfaces = [ "enp3s0f1" ];
 
   networking.firewall = {
     enable = true;
-    # 80 & 443 : nginx
-    # 554 : rstp (live tv)
-    allowedTCPPorts = [ 80 443 554];
-    allowedUDPPorts = [ 443 554 ]; 
+    trustedInterfaces = [ "enp3s0f1" ];
+    
+    allowedTCPPorts = [ 80 443 554 ];
+    allowedUDPPorts = [ 443 554 ];
   };
 }
